6251385cd7719d338a3bcfbb439ce03380c3d9b2
Jason Moggridge <github_hou1@jmoggr.com>
2025-02-11 11:45:45 -0500

    refactor: Update get_parent method and improve print logic in SemmetyFrame

--- src/SemmetyFrame.cpp
+++ src/SemmetyFrame.cpp
@@ -2,20 +2,22 @@
 #include "SemmetyFrame.hpp"
 
 
-SP<SemmetyParentFrame> SemmetyFrame::get_parent() const {
+SP<SemmetyFrame> SemmetyFrame::get_parent() const {
     return parent.lock();
 }
 
 
 void SemmetyFrame::print() const {
-    if (is_window()) {
+    if (data.is_window()) {
         // std::cout << "SemmetyFrame (WindowId: " << std::get<Window>(data).window << ")\n";
-    } else if (is_empty()) {
+    } else if (data.is_empty()) {
         // std::cout << "SemmetyFrame (Empty)\n";
     } else {
         // std::cout << "SemmetyFrame (Parent with children)\n";
-        for (const auto& child : std::get<SemmetyFrame::Parent>(data).children) {
-            child->print();
+        if (data.is_parent()) {
+            for (const auto& child : this->data.as_parent().children) {
+                child->print();
+            }
         }
     }
 }
