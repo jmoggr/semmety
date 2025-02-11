ba0bfe7c71bc0d51b07c654625b4d645b0a3f5f4
Jason Moggridge <github_hou1@jmoggr.com>
2025-02-11 17:10:00 -0500

    refactor: Simplify geometry propagation and standard window area calculations

--- src/SemmetyFrame.cpp
+++ src/SemmetyFrame.cpp
@@ -40,28 +40,6 @@ void SemmetyFrame::propagateGeometry(const std::optional<CBox>& geometry) {
     const auto& children = this->data.as_parent().children;
     children.front()->propagateGeometry(childGeometries.first);
     children.back()->propagateGeometry(childGeometries.second);
-
-	// // clang-format off
-	// static const auto p_gaps_in = ConfigValue<Hyprlang::CUSTOMTYPE, CCssGapData>("general:gaps_in");
-	// static const auto p_gaps_out = ConfigValue<Hyprlang::CUSTOMTYPE, CCssGapData>("general:gaps_out");
-	// static const auto group_inset = ConfigValue<Hyprlang::INT>("plugin:hy3:group_inset");
-	// static const auto tab_bar_height = ConfigValue<Hyprlang::INT>("plugin:hy3:tabs:height");
-	// static const auto tab_bar_padding = ConfigValue<Hyprlang::INT>("plugin:hy3:tabs:padding");
-
-	// auto workspace_rule = g_pConfigManager->getWorkspaceRuleFor(this->workspace);
-	// auto gaps_in = workspace_rule.gapsIn.value_or(*p_gaps_in);
-	// auto gaps_out = workspace_rule.gapsOut.value_or(*p_gaps_out);
-
-	// auto gap_topleft_offset = Vector2D(
-	//     (int) -(gaps_in.left - gaps_out.left),
-	//     (int) -(gaps_in.top - gaps_out.top)
-	// );
-
-	// auto gap_bottomright_offset = Vector2D(
-	// 		(int) -(gaps_in.right - gaps_out.right),
-	// 		(int) -(gaps_in.bottom - gaps_out.bottom)
-	// );
-	// // clang-format on    
 }
 
 std::pair<CBox, CBox> SemmetyFrame::getChildGeometries() const {
@@ -131,37 +109,37 @@ void SemmetyFrame::print() const {
 // 	}
 // }
 
-CBox SemmetyFrame::getStandardWindowArea(SBoxExtents extents) {
-	// static const auto p_gaps_in = ConfigValue<Hyprlang::CUSTOMTYPE, CCssGapData>("general:gaps_in");
+CBox SemmetyFrame::getStandardWindowArea(SBoxExtents extents, PHLWORKSPACE workspace) {
+	static const auto p_gaps_in = ConfigValue<Hyprlang::CUSTOMTYPE, CCssGapData>("general:gaps_in");
 
-	// auto workspace_rule = g_pConfigManager->getWorkspaceRuleFor(this->workspace);
-	// auto gaps_in = workspace_rule.gapsIn.value_or(*p_gaps_in);
+	auto workspace_rule = g_pConfigManager->getWorkspaceRuleFor(workspace);
+	auto gaps_in = workspace_rule.gapsIn.value_or(*p_gaps_in);
 
-	// SBoxExtents inner_gap_extents;
-	// inner_gap_extents.topLeft = {(int) -gaps_in.left, (int) -gaps_in.top};
-	// inner_gap_extents.bottomRight = {(int) -gaps_in.right, (int) -gaps_in.bottom};
+	SBoxExtents inner_gap_extents;
+	inner_gap_extents.topLeft = {(int) -gaps_in.left, (int) -gaps_in.top};
+	inner_gap_extents.bottomRight = {(int) -gaps_in.right, (int) -gaps_in.bottom};
 
-	// SBoxExtents combined_outer_extents;
-	// combined_outer_extents.topLeft = -this->gap_topleft_offset;
-	// combined_outer_extents.bottomRight = -this->gap_bottomright_offset;
+	SBoxExtents combined_outer_extents;
+	combined_outer_extents.topLeft = -this->gap_topleft_offset;
+	combined_outer_extents.bottomRight = -this->gap_bottomright_offset;
 
 	auto area = CBox(this->position, this->size);
-	// area.addExtents(inner_gap_extents);
-	// area.addExtents(combined_outer_extents);
+	area.addExtents(inner_gap_extents);
+	area.addExtents(combined_outer_extents);
 	area.addExtents(extents);
 
 	area.round();
 	return area;
 }
 
-void SemmetyFrame::applyRecursive() {
+void SemmetyFrame::applyRecursive(PHLWORKSPACE workspace) {
     if (this->data.is_empty()) {
         return;
     }
 
     if (this->data.is_parent()) {
         for (const auto& child : this->data.as_parent().children) {
-            child->applyRecursive();
+            child->applyRecursive(workspace);
         }
         return;
     }
@@ -181,23 +159,14 @@ void SemmetyFrame::applyRecursive() {
 
     window->unsetWindowData(PRIORITY_LAYOUT);
 
-    auto nodeBox = CBox(position, size);
-    nodeBox.round();
+    
+		auto reserved = window->getFullWindowReservedArea();
+		auto wb = getStandardWindowArea({-reserved.topLeft, -reserved.bottomRight}, workspace);
 
-    window->m_vSize = nodeBox.size() - Vector2D(10, 10);
-    window->m_vPosition = nodeBox.pos() - Vector2D(10, 10);
-
-    window->m_sWindowData.decorate = CWindowOverridableVar(true, PRIORITY_LAYOUT);
-    window->m_sWindowData.noBorder = CWindowOverridableVar(true, PRIORITY_LAYOUT);
-    window->m_sWindowData.noRounding = CWindowOverridableVar(true, PRIORITY_LAYOUT);
-    window->m_sWindowData.noShadow = CWindowOverridableVar(true, PRIORITY_LAYOUT);
-
+		*window->m_vRealPosition = wb.pos();
+		*window->m_vRealSize = wb.size();		
     window->updateWindowDecos();
 
-    const auto reserved = window->getFullWindowReservedArea();
-
-    *window->m_vRealPosition = window->m_vPosition + reserved.topLeft;
-    *window->m_vRealSize = window->m_vSize - (reserved.topLeft + reserved.bottomRight);
 
     window->sendWindowSize(true);
 }
