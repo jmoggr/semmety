cee2ae6742aee5ad77cf3d867fbe4ebb4812e61b
Jason Moggridge <github_hou1@jmoggr.com>
2025-02-12 09:00:33 -0500

    feat: Add new dispatchers for cycle_hidden, split, and remove actions

--- src/dispatchers.cpp
+++ src/dispatchers.cpp
@@ -7,42 +7,50 @@
 #include "globals.hpp"
 
 SDispatchResult dispatch_debug_v2(std::string arg) {
-    semmety_log(LOG, "Semmety Debug");
+	auto* workspace_wrapper = workspace_for_action(true);
+	if (workspace_wrapper == nullptr)
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
+
+        semmety_log(ERR, workspace_wrapper->root->print());
+
     return SDispatchResult{.passEvent = false, .success = true, .error = ""};
 }
 
 
-void dispatch_movefocus(std::string value) {
-	auto* workspace_wrapper = workspace_for_action(true);
-	if (workspace_wrapper == nullptr) return;
-
-
+SDispatchResult dispatch_movefocus(std::string value) {
+	// auto* workspace_wrapper = workspace_for_action(true);
+	// if (workspace_wrapper == nullptr) return;
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
 }
 
-void dispatch_split(std::string arg) {
+SDispatchResult split(std::string arg) {
 	auto* workspace_wrapper = workspace_for_action(true);
-	if (workspace_wrapper == nullptr) return;
+	if (workspace_wrapper == nullptr)
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
 
 	auto focused_frame = workspace_wrapper->getFocusedFrame();
 
   focused_frame->data = SemmetyFrame::Parent(focused_frame, std::move(focused_frame->data), SemmetyFrame::Empty{});
+    workspace_wrapper->apply();
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
 }
 
-void dispatch_remove(std::string arg) {
+SDispatchResult dispatch_remove(std::string arg) {
     auto* workspace_wrapper = workspace_for_action(true);
-    if (workspace_wrapper == nullptr) return;
+    if (workspace_wrapper == nullptr)
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
 
     auto focused_frame = workspace_wrapper->getFocusedFrame();
 
     if (!focused_frame->data.is_leaf()) {
         semmety_log(ERR, "Can only remove leaf frames");
-        return;
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
     }
 
     auto parent = focused_frame->get_parent();
     if (!parent) {
-        semmety_log(DEBUG, "Frame has no parent, cannot remove the root frame!");
-        return;
+        semmety_log(ERR, "Frame has no parent, cannot remove the root frame!");
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
     }
 
     auto& siblings = parent->data.as_parent().children;
@@ -60,8 +68,40 @@ void dispatch_remove(std::string arg) {
     }
 
     workspace_wrapper->setFocusedFrame(parent);
+    workspace_wrapper->apply();
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
+}
+
+SDispatchResult cycle_hidden(std::string arg) {
+        semmety_log(ERR, "cycle hidden");
+    auto* workspace_wrapper = workspace_for_action(true);
+    if (workspace_wrapper == nullptr) {
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
+
+}    auto focused_frame = workspace_wrapper->getFocusedFrame();
+    
+    if (!focused_frame->data.is_leaf()) {
+        semmety_log(ERR, "Can only remove leaf frames");
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
+    }
+    
+    if (workspace_wrapper->minimized_windows.empty()) {
+        return SDispatchResult{.passEvent = false, .success = true, .error = ""};
+    }    
+
+    if (focused_frame->data.is_window()) {
+        workspace_wrapper->minimized_windows.push_back(focused_frame->data.as_window());
+    } 
+
+    focused_frame->data = workspace_wrapper->minimized_windows.front();
+    workspace_wrapper->minimized_windows.pop_front();
+    workspace_wrapper->apply();
+    return SDispatchResult{.passEvent = false, .success = true, .error = ""};
 }
 
 void registerDispatchers() {
     HyprlandAPI::addDispatcherV2(PHANDLE, "semmety:debug", dispatch_debug_v2);
+    HyprlandAPI::addDispatcherV2(PHANDLE, "semmety:cycle_hidden", cycle_hidden);
+    HyprlandAPI::addDispatcherV2(PHANDLE, "semmety:split", split);
+    HyprlandAPI::addDispatcherV2(PHANDLE, "semmety:remove", dispatch_remove);
 }
