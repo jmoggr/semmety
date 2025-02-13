15161755b7ac9599b4a5eff05416841db2f22a93
Jason Moggridge <github_hou1@jmoggr.com>
2025-02-13 13:43:52 -0500

    feat: Add semmety_critical_error function for logging critical errors

--- src/log.hpp
+++ src/log.hpp
@@ -7,3 +7,11 @@ void semmety_log(eLogLevel level, std::format_string<Args...> fmt, Args&&... arg
 	auto msg = std::vformat(fmt.get(), std::make_format_args(args...));
 	Debug::log(level, "[semmety] {}", msg);
 }
+
+template <typename... Args>
+std::unique_ptr<std::runtime_error> semmety_critical_error(std::format_string<Args...> fmt, Args&&... args) {
+	auto msg = std::vformat(fmt.get(), std::make_format_args(args...));
+	Debug::log(CRIT, "[semmety] {}", msg);
+  return std::make_unique<std::runtime_error>(msg);
+}
+
