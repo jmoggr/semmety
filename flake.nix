62d445efb93715fcfd52fec8f70cb74bd41fa2c0
Jason Moggridge <github_hou1@jmoggr.com>
2025-03-03 19:43:17 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -10,80 +10,107 @@
     };
     hyprland = {
 
-      url =  "github:hyprwm/Hyprland/68bb3e7";
+      url = "github:hyprwm/Hyprland/68bb3e7";
       inputs.systems.follows = "systems";
     };
   };
 
-  outputs = {
-    self,
-    systems,
-    hyprland,
-    ...
-  }: let
-    inherit (builtins) concatStringsSep elemAt head readFile split substring;
-    inherit (hyprland.inputs) nixpkgs;
+  outputs =
+    {
+      self,
+      systems,
+      hyprland,
+      ...
+    }:
+    let
+      inherit (builtins)
+        concatStringsSep
+        elemAt
+        head
+        readFile
+        split
+        substring
+        ;
+      inherit (hyprland.inputs) nixpkgs;
 
-    perSystem = attrs:
-      nixpkgs.lib.genAttrs (import systems) (system:
-        attrs system (import nixpkgs {
-          inherit system;
-          overlays = [hyprland.overlays.hyprland-packages];
-        }));
+      perSystem =
+        attrs:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          attrs system (
+            import nixpkgs {
+              inherit system;
+              overlays = [ hyprland.overlays.hyprland-packages ];
+            }
+          )
+        );
 
-    # Generate version
-    mkDate = longDate: (concatStringsSep "-" [
-      (substring 0 4 longDate)
-      (substring 4 2 longDate)
-      (substring 6 2 longDate)
-    ]);
+      # Generate version
+      mkDate =
+        longDate:
+        (concatStringsSep "-" [
+          (substring 0 4 longDate)
+          (substring 4 2 longDate)
+          (substring 6 2 longDate)
+        ]);
 
-    version =
-      (head (split "'"
-        (elemAt
-          (split " version: '" (readFile ./meson.build))
-          2)))
-      + "+date=${mkDate (self.lastModifiedDate or "19700101")}_${self.shortRev or "dirty"}";
-  in {
-    packages = perSystem (system: pkgs: {
-      Hyprspace = let
-        hyprlandPkg = hyprland.packages.${system}.hyprland-debug;
-      in
-        pkgs.gcc14Stdenv.mkDerivation {
-          pname = "Semmety";
-          inherit version;
-          src = ./.;
+      version =
+        (head (split "'" (elemAt (split " version: '" (readFile ./meson.build)) 2)))
+        + "+date=${mkDate (self.lastModifiedDate or "19700101")}_${self.shortRev or "dirty"}";
+    in
+    {
+      packages = perSystem (
+        system: pkgs: {
+          Hyprspace =
+            let
+              hyprlandPkg = hyprland.packages.${system}.hyprland-debug;
+            in
+            pkgs.gcc14Stdenv.mkDerivation {
+              pname = "Semmety";
+              inherit version;
+              src = ./.;
 
-          nativeBuildInputs = hyprlandPkg.nativeBuildInputs;
-          buildInputs = [hyprlandPkg] ++ hyprlandPkg.buildInputs;
-          dontUseCmakeConfigure = true;
+              nativeBuildInputs = hyprlandPkg.nativeBuildInputs;
+              buildInputs = [ hyprlandPkg ] ++ hyprlandPkg.buildInputs;
+              dontUseCmakeConfigure = true;
 
-          meta = with pkgs.lib; {
-            homepage = "https://github.com/jmoggr/semmety";
-            description = "Semmit automatic tiling layout plugin for Hyprland";
-            license = licenses.gpl2Only;
-            platforms = platforms.linux;
+              meta = with pkgs.lib; {
+                homepage = "https://github.com/jmoggr/semmety";
+                description = "Semmit automatic tiling layout plugin for Hyprland";
+                license = licenses.gpl2Only;
+                platforms = platforms.linux;
+              };
+            };
+          default = self.packages.${system}.Hyprspace;
+        }
+      );
+
+      devShells = perSystem (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            name = "Semmety-shell";
+            nativeBuildInputs = with pkgs; [
+              gcc14
+              clang-tools
+              meson
+              ninja
+              pkg-config
+              libdrm
+              fmt
+            ];
+            buildInputs = [ hyprland.packages.${system}.hyprland ];
+            inputsFrom = [
+              hyprland.packages.${system}.hyprland-debug
+              self.packages.${system}.Hyprspace
+            ];
+            # shellHook = ''
+            #   meson setup build --reconfigure
+            #   sed -e 's/c++23/c++2b/g' ./build/compile_commands.json > ./compile_commands.json
+            # '';
           };
-        };
-      default = self.packages.${system}.Hyprspace;
-    });
+        }
+      );
 
-    devShells = perSystem (system: pkgs: {
-      default = pkgs.mkShell {
-        name = "Semmety-shell";
-        nativeBuildInputs = with pkgs; [gcc14 clang-tools meson ninja pkg-config libdrm fmt];
-        buildInputs = [hyprland.packages.${system}.hyprland];
-        inputsFrom = [
-          hyprland.packages.${system}.hyprland-debug
-          self.packages.${system}.Hyprspace
-        ];
-        shellHook = ''
-          meson setup build --reconfigure
-          sed -e 's/c++23/c++2b/g' ./build/compile_commands.json > ./compile_commands.json
-        '';
-      };
-    });
-
-    formatter = perSystem (_: pkgs: pkgs.alejandra);
-  };
+      formatter = perSystem (_: pkgs: pkgs.alejandra);
+    };
 }
